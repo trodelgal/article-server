dbc9b52f5fe9f7cc51b13686b2b32049b6279118
Gal Trodel <galtrudel@gmail.com>
2024-12-29 16:07:31 +0200

    Setup docker for prod and dev, connect to mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
 import app from "./app";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+
+// Connection URL
+const mongoURI = process.env.MONGO_URL || ""; // Change this to your MongoDB URI
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
